37e72ac9252cd021bb0bee5be7548a9c59f17d4e
Crislâine Santos <criis.santos@outlook.com>
2022-04-05 14:49:38 -0300

    feat: working with core components

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title='TAP ME'/>
     </View>
+
   );
 }
 
