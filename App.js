aa7d8e65067f70c68113c1b8df5d8bf11c71f16c
Crislâine Santos <criis.santos@outlook.com>
2022-04-05 16:54:38 -0300

    feat: handling events

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [goals, setGoals] = useState([])
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler() {
+    setGoals(prevState => (
+      [...prevState, enteredGoalText]
+    ))
+    console.log(goals)
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add Goal' />
+        <TextInput style={styles.textInput} placeholder='Your course goal!'
+          onChangeText={goalInputHandler}
+        />
+        <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
