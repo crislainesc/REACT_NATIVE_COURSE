0b8b7f32ec0ea92089d0edbb23f818d4fdc9b39b
Crislâine Santos <criis.santos@outlook.com>
2022-04-05 16:28:21 -0300

    style: improving the layout

--- App.js
+++ App.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-around',
-        alignItems: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title='Add Goal' />
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 2,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 3,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
       </View>
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    padding: 50,
+    paddingHorizontal: 16,
+  },
+
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#7ce',
+    width: '70%',
+    marginRight: 8,
+    padding: 5
+  },
+
+  goalsContainer: {
+    flex: 4,
+  }
+})
