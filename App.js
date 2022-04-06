ef7e064ec439417807002657499ffefdf771b837
Crislâine Santos <criis.santos@outlook.com>
2022-04-06 20:54:24 -0300

    feat: adding a modal screen

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
+	const [modalIsVisible, setModalIsVisible] = useState(false);
 	const [goals, setGoals] = useState([]);
+
+	function startAddGoalHandler() {
+		setModalIsVisible(true);
+	}
 
 	function addGoalHandler(enteredGoalText) {
 		setGoals((currentGoals) => [
 			...currentGoals,
 			{ text: enteredGoalText, id: Math.random().toString() },
 		]);
+    setModalIsVisible(false)
 	}
 
 	function deleteGoalHandler(id) {
@@ -22,7 +28,12 @@ export default function App() {
 
 	return (
 		<View style={styles.appContainer}>
-			<GoalInput onAddGoal={addGoalHandler} />
+			<Button
+				title='Add New Goal'
+				color='#5e0acc'
+				onPress={startAddGoalHandler}
+			/>
+			<GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible} />
 			<View style={styles.goalsContainer}>
 				<FlatList
 					data={goals}
