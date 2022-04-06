75c242816124b4131b55891b6cd060de9e663668
Crislâine Santos <criis.santos@outlook.com>
2022-04-06 19:49:44 -0300

    refactor: optimizing lists with flatlist

--- App.js
+++ App.js
@@ -1,82 +1,97 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	ScrollView,
+	FlatList,
+} from 'react-native';
 
 export default function App() {
+	const [enteredGoalText, setEnteredGoalText] = useState('');
+	const [goals, setGoals] = useState([]);
 
-  const [enteredGoalText, setEnteredGoalText] = useState('')
-  const [goals, setGoals] = useState([])
+	function goalInputHandler(enteredText) {
+		setEnteredGoalText(enteredText);
+	}
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText)
-  }
+	function addGoalHandler() {
+		setGoals((currentGoals) => [
+			...currentGoals,
+			{ text: enteredGoalText, id: Math.random().toString() },
+		]);
+		setEnteredGoalText('');
+	}
 
-  function addGoalHandler() {
-    setGoals(currentGoals => (
-      [...currentGoals, enteredGoalText]
-    ))
-    setEnteredGoalText('')
-  }
-
-  return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!'
-          onChangeText={goalInputHandler}
-          value={enteredGoalText}
-        />
-        <Button title='Add Goal' onPress={addGoalHandler} />
-      </View>
-      <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false }>
-          {goals.map((goal) => (
-            <View key={goal} style={styles.goalItem}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
-    </View>
-
-  );
+	return (
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder='Your course goal!'
+					onChangeText={goalInputHandler}
+					value={enteredGoalText}
+				/>
+				<Button title='Add Goal' onPress={addGoalHandler} />
+			</View>
+			<View style={styles.goalsContainer}>
+				<FlatList
+					data={goals}
+					renderItem={(itemData) => {
+						return (
+							<View style={styles.goalItem}>
+								<Text style={styles.goalText}>{itemData.item.text}</Text>
+							</View>
+						);
+					}}
+					keyExtractor={(item, index) => {
+						return item.id;
+					}}
+					alwaysBounceVertical={false}></FlatList>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    padding: 50,
-    paddingHorizontal: 16,
-  },
+	appContainer: {
+		flex: 1,
+		padding: 50,
+		paddingHorizontal: 16,
+	},
 
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-  },
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: '#ccc',
+	},
 
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#7ce',
-    width: '70%',
-    marginRight: 8,
-    padding: 5
-  },
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#7ce',
+		width: '70%',
+		marginRight: 8,
+		padding: 5,
+	},
 
-  goalsContainer: {
-    flex: 4,
-  },
+	goalsContainer: {
+		flex: 4,
+	},
 
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 8,
-    backgroundColor: '#5e0acc',
-  },
+	goalItem: {
+		margin: 8,
+		padding: 8,
+		borderRadius: 8,
+		backgroundColor: '#5e0acc',
+	},
 
-  goalText: {
-    color: 'white'
-  }
-})
+	goalText: {
+		color: 'white',
+	},
+});
