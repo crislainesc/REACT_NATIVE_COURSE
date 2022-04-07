96a464fc3c1b0f903c89ac991d0e1e6b7cab8c40
Crislâine Santos <criis.santos@outlook.com>
2022-04-06 21:45:41 -0300

    debugging javascript remotely

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -28,8 +28,7 @@ function GoalInput(props) {
 			<View style={styles.inputContainer}>
 				<Image
 					style={styles.image}
-					// source={require('../assets/images/goal.png')}
-               source='/some/path/to/image'
+					source={require('../assets/images/goal.png')}
 				/>
 				<TextInput
 					style={styles.textInput}
