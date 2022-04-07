53bea61f3940039a2beec089048171d223315321
Crislâine Santos <criis.santos@outlook.com>
2022-04-07 13:48:55 -0300

    style: improving the game screen visuals

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
 import PrimaryButton from '../components/ui/PrimaryButton';
 
 import Title from '../components/ui/Title';
@@ -62,8 +64,8 @@ function GameScreen({ userNumber, onGameOver }) {
 		<View style={styles.screen}>
 			<Title style={styles.title}>Opponent's Guess</Title>
 			<NumberContainer>{currentGuess}</NumberContainer>
-			<View>
-				<Text>Higher or lower?</Text>
+			<Card>
+				<InstructionText>Higher or lower?</InstructionText>
 				<View>
 					<PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
 						+
@@ -72,7 +74,7 @@ function GameScreen({ userNumber, onGameOver }) {
 						-
 					</PrimaryButton>
 				</View>
-			</View>
+			</Card>
 			{/* <View>LOG ROUNDS</View> */}
 		</View>
 	);
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { View, TextInput, StyleSheet, Alert } from 'react-native';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
+import Title from '../components/ui/Title';
 
 import Colors from '../constants/colors';
 
@@ -33,24 +36,28 @@ function StartGameScreen({ onPickNumber }) {
 	}
 
 	return (
-		<View style={styles.inputContainer}>
-			<TextInput
-				style={styles.numberInput}
-				maxLength={2}
-				keyboardType='number-pad'
-				autoCapitalize='none'
-				autoCorrect={false}
-				onChangeText={numberInputHandler}
-				value={enteredNumber}
-			/>
-			<View style={styles.buttonsContainer}>
-				<View style={styles.buttonContainer}>
-					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+		<View style={styles.rootContainer}>
+			<Title>Guess My Number</Title>
+			<Card>
+				<InstructionText>Enter a Number</InstructionText>
+				<TextInput
+					style={styles.numberInput}
+					maxLength={2}
+					keyboardType='number-pad'
+					autoCapitalize='none'
+					autoCorrect={false}
+					onChangeText={numberInputHandler}
+					value={enteredNumber}
+				/>
+				<View style={styles.buttonsContainer}>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+					</View>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+					</View>
 				</View>
-				<View style={styles.buttonContainer}>
-					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-				</View>
-			</View>
+			</Card>
 		</View>
 	);
 }
@@ -58,19 +65,10 @@ function StartGameScreen({ onPickNumber }) {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-	inputContainer: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		padding: 16,
-		marginHorizontal: 24,
+	rootContainer: {
+		flex: 1,
 		marginTop: 100,
-		backgroundColor: Colors.primary800,
-		borderRadius: 8,
-		elevation: 4,
-		shadowColor: 'black',
-		shadowOffset: { width: 0, height: 2 },
-		shadowRadius: 6,
-		shadowOpacity: 0.25,
+		alignItems: 'center'
 	},
 
 	numberInput: {
