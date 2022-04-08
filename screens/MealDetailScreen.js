c90748831163e9e70f50e4428c170689ef62efbd
Crislâine Santos <criis.santos@outlook.com>
2022-04-08 17:46:42 -0300

    feat: using the created context with usecontext

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,81 +1,91 @@
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useContext } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+
+import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 import IconButton from '../components/IconButton';
 import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
-import { MEALS } from '../data/dummy-data';
 
 function MealDetailScreen({ route, navigation }) {
-  const mealId = route.params.mealId;
+	const favoritesMealsContext = useContext(FavoritesContext);
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+	const mealId = route.params.mealId;
 
-  function headerButtonPressHandler() {
-    console.log('Pressed!');
-  }
+	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => {
-        return (
-          <IconButton
-            icon="star"
-            color="white"
-            onPress={headerButtonPressHandler}
-          />
-        );
-      },
-    });
-  }, [navigation, headerButtonPressHandler]);
+	const mealIsFavorite = favoritesMealsContext.ids.includes(mealId);
 
-  return (
-    <ScrollView style={styles.rootContainer}>
-      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
-      <Text style={styles.title}>{selectedMeal.title}</Text>
-      <MealDetails
-        duration={selectedMeal.duration}
-        complexity={selectedMeal.complexity}
-        affordability={selectedMeal.affordability}
-        textStyle={styles.detailText}
-      />
-      <View style={styles.listOuterContainer}>
-        <View style={styles.listContainer}>
-          <Subtitle>Ingredients</Subtitle>
-          <List data={selectedMeal.ingredients} />
-          <Subtitle>Steps</Subtitle>
-          <List data={selectedMeal.steps} />
-        </View>
-      </View>
-    </ScrollView>
-  );
+	function changeFavoritesStatusHandler() {
+		if (!mealIsFavorite) {
+			favoritesMealsContext.addFavorite(selectedMeal.id);
+		} else {
+			favoritesMealsContext.removeFavorite(selectedMeal.id);
+		}
+	}
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerRight: () => {
+				return (
+					<IconButton
+						icon={`${mealIsFavorite ? 'star' : 'star-outline'}`}
+						color='white'
+						onPress={changeFavoritesStatusHandler}
+					/>
+				);
+			},
+		});
+	}, [navigation, changeFavoritesStatusHandler]);
+
+	return (
+		<ScrollView style={styles.rootContainer}>
+			<Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+			<Text style={styles.title}>{selectedMeal.title}</Text>
+			<MealDetails
+				duration={selectedMeal.duration}
+				complexity={selectedMeal.complexity}
+				affordability={selectedMeal.affordability}
+				textStyle={styles.detailText}
+			/>
+			<View style={styles.listOuterContainer}>
+				<View style={styles.listContainer}>
+					<Subtitle>Ingredients</Subtitle>
+					<List data={selectedMeal.ingredients} />
+					<Subtitle>Steps</Subtitle>
+					<List data={selectedMeal.steps} />
+				</View>
+			</View>
+		</ScrollView>
+	);
 }
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    marginBottom: 32,
-  },
-  image: {
-    width: '100%',
-    height: 350,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 24,
-    margin: 8,
-    textAlign: 'center',
-    color: 'white',
-  },
-  detailText: {
-    color: 'white',
-  },
-  listOuterContainer: {
-    alignItems: 'center',
-  },
-  listContainer: {
-    width: '80%',
-  },
+	rootContainer: {
+		marginBottom: 32,
+	},
+	image: {
+		width: '100%',
+		height: 350,
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 24,
+		margin: 8,
+		textAlign: 'center',
+		color: 'white',
+	},
+	detailText: {
+		color: 'white',
+	},
+	listOuterContainer: {
+		alignItems: 'center',
+	},
+	listContainer: {
+		width: '80%',
+	},
 });
