35108d3190c0a8b5ea3146799a527a5e1126e8b0
Crislâine Santos <criis.santos@outlook.com>
2022-04-08 15:35:43 -0300

    feat: adding drawer navigation & creating a drawer

--- App.js
+++ App.js
@@ -1,49 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDetailsScreen from './screens/MealDetailsScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 	return (
-		<>
-			<StatusBar style='light' />
-			<NavigationContainer>
-				<Stack.Navigator
-					screenOptions={{
-						headerStyle: { backgroundColor: '#351401' },
-						headerTintColor: 'white',
-						contentStyle: { backgroundColor: '#3f2f25' },
-					}}>
-					<Stack.Screen
-						name='MealsCategories'
-						component={CategoriesScreen}
-						options={{
-							headerTitle: 'All Caregories',
-						}}
-					/>
-					<Stack.Screen
-						name='MealsOverview'
-						component={MealsOverviewScreen}
-						// options={({ route, navigation }) => {
-						// 	const { categoryId } = route.params;
-						// 	return {
-						// 		title: categoryId
-						// 	};
-						// }}
-					/>
-					<Stack.Screen name='MealDetail' component={MealDetailsScreen} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		</>
+		<NavigationContainer>
+			<Drawer.Navigator>
+				<Drawer.Screen name='Welcome' component={WelcomeScreen} />
+				<Drawer.Screen name='User' component={UserScreen} />
+			</Drawer.Navigator>
+		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {},
-});
