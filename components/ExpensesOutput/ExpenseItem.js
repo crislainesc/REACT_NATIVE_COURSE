404ed1c20c4c37782e33851726df2194a78e7845
Crislâine Santos <criis.santos@outlook.com>
2022-04-09 14:12:03 -0300

    feat: support for different editing modes and added route parameters

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -5,11 +5,13 @@ import { getFormattedDate } from '../../utils/date';
 
 import { GlobalStyles } from '../../styles';
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
 	const navigation = useNavigation();
 
 	function expensePressHandler() {
-		navigation.navigate('ManageExpense')
+		navigation.navigate('ManageExpense', {
+			expenseId: id,
+		});
 	}
 
 	return (
