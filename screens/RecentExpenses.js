ce94f732b73f233fbcb56b36af5646bb62e3521d
Crislâine Santos <criis.santos@outlook.com>
2022-04-09 22:52:32 -0300

    feat: loaded expenses added in RecentExpenses screen and expenses context

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,12 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { ExpensesContext } from '../store/expenses-context';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { getDateMinusDays } from '../utils/getDateMinusDays';
+import { fetchExpenses } from '../shared/services';
 
 function RecentExpenses() {
 	const expensesContext = useContext(ExpensesContext);
+
+	useEffect(() => {
+		async function getExpenses() {
+			const expenses = await fetchExpenses();
+			expensesContext.setExpenses(expenses);
+		}
+
+		getExpenses();
+	}, []);
 
 	const recentExpenses = expensesContext.expenses.filter((expense) => {
 		const today = new Date();
