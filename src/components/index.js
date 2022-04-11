cdd472da28cb887e5aa52719cef2b18610af52ff
Crislâine Santos <criis.santos@outlook.com>
2022-04-11 17:28:19 -0300

    feat: import and export of components in an index.js

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import ImagePicked from './Places/ImagePicked';
+import PlaceForm from './Places/PlaceForm';
+import PlaceItem from './Places/PlaceItem';
+import PlacesList from './Places/PlacesList';
+
+import IconButton from './UI/IconButton';
+import OutlinedButton from './UI/OutlinedButton';
+import Button from './UI/Button';
+
+export {
+	ImagePicked,
+	PlaceForm,
+	PlaceItem,
+	PlacesList,
+	IconButton,
+	OutlinedButton,
+	Button,
+};
