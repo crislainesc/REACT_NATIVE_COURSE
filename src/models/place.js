253457688f2213f5c9590368d25a95414d814d9b
Crislâine Santos <criis.santos@outlook.com>
2022-04-11 17:28:44 -0300

    feat: Place model added

---
+++ src/models/place.js
@@ -0,0 +1,12 @@
+export class Place {
+	constructor(title, imageUri, location, id) {
+		this.title = title;
+		this.imageUri = imageUri;
+		this.address = location.address;
+		this.location = {
+			latitude: location.latitude,
+			longitude: location.longitude,
+		};
+		this.id = id;
+	}
+}
