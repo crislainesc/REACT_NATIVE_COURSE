e243d2a09c1801b24e20747dc9303802fd9e14c6
Crislâine Santos <criis.santos@outlook.com>
2022-04-11 17:33:19 -0300

    feat: import and export of constants and utils in an index.js

---
+++ src/shared/index.js
@@ -0,0 +1,20 @@
+import { Colors } from './constants/colors';
+
+import { getMapPreview, getAddres } from './utils/location';
+
+import {
+	init,
+	insertPlace,
+	fetchPlaces,
+	fetchPlaceDetails,
+} from './utils/database';
+
+export {
+	Colors,
+	getMapPreview,
+	getAddres,
+	init,
+	insertPlace,
+	fetchPlaces,
+	fetchPlaceDetails,
+};
